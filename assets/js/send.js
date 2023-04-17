60bc079600909dfdd48546412805c31e2f58b64e
Prado <peu_prado01@hotmail.com>
2023-04-17 05:26:22 -0300

    feat: Adiciona função de receber email

---
+++ assets/js/send.js
@@ -0,0 +1,24 @@
+const sendEmail = document.getElementById('send');
+const nome = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+function validations() {
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+    if (nome.value.length <= 2) {
+        alert('Nome tem que ter mais que três caracteres');
+    }
+    if (!emailRegex.test(email.value)) {
+        alert('Email incorreto');
+    }
+    if (message.value.length <= 6) {
+        alert('Mensagem menor que 6 caracteres')
+    } else {
+        alert('Enviado com sucesso')
+    }
+}
+sendEmail.addEventListener('click', (event) => {
+    // event.preventDefault();
+    validations();
+});
