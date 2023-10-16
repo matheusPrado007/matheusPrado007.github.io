e4e4ae348fb46742015882332abff4442da1f8af
Prado <peu_prado01@hotmail.com>
2023-10-16 08:14:01 -0300

    adiciona img em sobre mim

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,23 +16,5 @@ AOS.init({
 
 
 
-  // $(document).ready(function () {
-  //   $("#projetos .row").owlCarousel({
-  //     loop: true,
-  //     margin: 30,
-  //     nav: true,
-  //     responsive: {
-  //       0: {
-  //         items: 1,
-  //       },
-  //       768: {
-  //         items: 2,
-  //       },
-  //       992: {
-  //         items: 3,
-  //       },
-  //     },
-  //   });
-  // });
 
 
